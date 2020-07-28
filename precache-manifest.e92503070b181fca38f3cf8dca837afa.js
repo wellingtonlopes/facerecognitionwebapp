b67e4a68b1cfa34c9b1548dcb7b32b3a3d4691e3
wellingtonlopes <ookami.seta@gmail.com>
2020-07-28 17:30:16 -0400

    Updates

---
+++ precache-manifest.e92503070b181fca38f3cf8dca837afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db427b52f1a94e39e8d724b79ab03be3",
+    "url": "/facerecognitionwebapp/index.html"
+  },
+  {
+    "revision": "bf9e75ffb31604f61110",
+    "url": "/facerecognitionwebapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4a6c464a98c35b08b435",
+    "url": "/facerecognitionwebapp/static/css/main.b7c94451.chunk.css"
+  },
+  {
+    "revision": "bf9e75ffb31604f61110",
+    "url": "/facerecognitionwebapp/static/js/2.c27cf9a2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionwebapp/static/js/2.c27cf9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6c464a98c35b08b435",
+    "url": "/facerecognitionwebapp/static/js/main.201d1c58.chunk.js"
+  },
+  {
+    "revision": "5be272450acd46208803",
+    "url": "/facerecognitionwebapp/static/js/runtime-main.9c111681.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/facerecognitionwebapp/precache-manifest.e92503070b181fca38f3cf8dca837afa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facerecognitionwebapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
